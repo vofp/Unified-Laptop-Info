9312ed7f43a08be05660e3859db1470b2581df35
vofp <vof@onid.orst.edu>
2013-03-09 07:57:16 -0800

    fix rakuten test

--- test/parse_rakuten.rb
+++ test/parse_rakuten.rb
@@ -1,4 +1,4 @@
-require "../src/parse/rakuten"
+require File.join(File.dirname(__FILE__), "../src/parse/rakuten")
 require "test/unit"
 
 $specs_rakuten = {"Hard Drive Capacity"=>"500 GB", "Product Type"=>"Ultrabook", "Height"=>"0.8\"", "Green Compliance Certificate/Authority"=>"RoHS", "Graphics Controller Model"=>"HD 4000", "Green Compliant"=>"Yes", "Memory Technology"=>"DDR3 SDRAM", "Product Series"=>"S400", "Display Screen Type"=>"Active Matrix TFT Color LCD", "Total Number of USB Ports"=>"3", "Operating System Architecture"=>"64-bit", "Processor Type"=>"Core i5", "Processor Speed"=>"1.70 GHz", "Package Contents"=>"VivoBook S400CA-DH51T UltrabookLithium Ion BatteryAC Adapter", "Touchscreen"=>"Yes", "Screen Mode"=>"HD", "Screen Size"=>"14.1\"", "HDMI"=>"Yes", "Operating System"=>"Genuine Windows 8", "Cache"=>"3 MB", "Width"=>"13.3\"", "Weight (Approximate)"=>"4.40 lb", "Wi-Fi"=>"Yes", "Brand Name"=>"Asus", "Screen Resolution"=>"1366 x 768", "Webcam"=>"Yes", "Processor Manufacturer"=>"Intel", "Solid State Drive Capacity"=>"24 GB", "Color"=>"Black", "Product Name"=>"VivoBook S400CA-DH51T Ultrabook", "Manufacturer"=>"ASUS Computer International", "Manufacturer Part Number"=>"S400CA-DH51T", "Graphics Controller Manufacturer"=>"Intel", "Processor Core"=>"Dual-core (2 Core)", "Wi-Fi Standard"=>"IEEE 802.11b/g/n", "Graphics Memory Accessibility"=>"Shared", "Name"=>"Asus VivoBook S400CA-DH51T 14.1\" HD Touchscreen Ultrabook, Intel Cor i5-3317U, 4GB RAM, 500GB HDD+24GB SSD, Windows 8", "64-bit Processing"=>"Yes", "Depth"=>"9.4\"", "Battery Chemistry"=>"Lithium Ion (Li-Ion)", "Processor Model"=>"i5-3317U", "Backlight Technology"=>"LED", "Aspect Ratio"=>"16:9", "Number of Cells"=>"3-cell", "Standard Memory"=>"4 GB", "Product Line"=>"VivoBook", "Product Model"=>"S400CA-DH51T", "Manufacturer Website Address"=>"usa.asus.com"}
@@ -24,7 +24,7 @@ class TestParseRakuten < Test::Unit::TestCase
     end
 
     def test_organizer
-        p output = organizer_rakuten($specs_rakuten)
+        output = organizer_rakuten($specs_rakuten)
         assert_equal([["1.70"]],output[:cpu_speed])
         assert_equal([["1366", "768"]],output[:resolution])
         assert_equal([["500", "gb"], ["24", "gb"]],output[:hd_size])
